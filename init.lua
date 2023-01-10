6a77147b0da9008834c4184e8629524c8db71e23
Jake Scott <scottj1123@gmail.com>
2023-01-10 15:55:24 -0800

    minor refactoring

--- init.lua
+++ init.lua
@@ -6,16 +6,13 @@
 --   |_|_| |_|_|\__(_)_|\__,_|\__,_|
 --
 -- ====================================
--- Resources:
--- https://github.com/nanotee/nvim-lua-guide
--- https://github.com/nvim-lua/kickstart.nvim
--- https://www.youtube.com/playlist?list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ
+-- Inspired by https://github.com/nvim-lua/kickstart.nvim
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 -- General Settings
 -- vim.g.netrw_banner = 0
-
 vim.cmd([[
 set mouse=a
 set number relativenumber
@@ -45,37 +42,38 @@ set nowrap
 ]])
 
 -- Keymaps ---------------------------------------------------------------------------------------
-vim.keymap.set('n', '<leader>ce', ':edit $MYVIMRC<cr>')
-vim.keymap.set('n', '<leader>cr', ':source $MYVIMRC<cr>:echo "init.lua reloaded"<cr>')
-vim.keymap.set('n', '<leader>c/', ':nohlsearch<cr> :echo "Search cleared."<cr>')
-vim.keymap.set('n', '<leader>cn', ':set relativenumber!<cr>')
-vim.keymap.set('n', '<leader>cs', ':setlocal spell! spelllang=en_us<cr>')
--- vim.keymap.set('n', '<leader>cf', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>r', ':!<up><cr>', { desc = 'Run last external program' })
-vim.keymap.set('n', '<leader>y', '"+y')
--- vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('v', '<leader>y', '"+y')
--- vim.keymap.set('v', '<leader>p', '"+p')
-vim.keymap.set('n', '<leader>dd', '^D')
-vim.keymap.set('n', '<leader>\\', ':vsp<cr>')
-vim.keymap.set('n', '<leader>-', ':sp<cr>')
-vim.keymap.set('n', '<leader>*', ':%s/\\<<c-r><c-w>\\>//g<left><left>', { desc = 'Search & replace word under cursor' })
-vim.keymap.set('o', 'fun', ':<c-u>normal! 0f(hviw<cr>', { desc = 'Change function name' })
+keymap = vim.keymap.set
+keymap('n', '<leader>ce', ':edit $MYVIMRC<cr>')
+keymap('n', '<leader>cr', ':source $MYVIMRC<cr>:echo "init.lua reloaded"<cr>')
+keymap('n', '<leader>c/', ':nohlsearch<cr> :echo "Search cleared."<cr>')
+keymap('n', '<leader>cn', ':set relativenumber!<cr>')
+keymap('n', '<leader>cs', ':setlocal spell! spelllang=en_us<cr>')
+-- map('n', '<leader>cf', vim.cmd.Ex)
+keymap('n', '<leader>r', ':!<up><cr>', { desc = 'Run last external program' })
+keymap('n', '<leader>y', '"+y')
+-- map('n', '<leader>p', '"+p')
+keymap('v', '<leader>y', '"+y')
+-- map('v', '<leader>p', '"+p')
+keymap('n', '<leader>dd', '^D')
+keymap('n', '<leader>\\', ':vsp<cr>')
+keymap('n', '<leader>-', ':sp<cr>')
+keymap('n', '<leader>*', ':%s/\\<<c-r><c-w>\\>//g<left><left>', { desc = 'Search & replace word under cursor' })
+keymap('o', 'fun', ':<c-u>normal! 0f(hviw<cr>', { desc = 'Change function name' })
 
 
-vim.keymap.set('v', "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', "K", ":m '<-2<CR>gv=gv")
+keymap('v', "J", ":m '>+1<CR>gv=gv")
+keymap('v', "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'Y', 'yg$')
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'nzzzv')
+keymap('n', 'Y', 'yg$')
+keymap('n', 'J', 'mzJ`z')
+keymap('n', '<C-d>', '<C-d>zz')
+keymap('n', '<C-u>', '<C-u>zz')
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'nzzzv')
 
-vim.keymap.set('x', '<leader>p', '"_dP')
+keymap('x', '<leader>p', '"_dP')
 
-vim.keymap.set('n', 'Q', '<nop>')
+keymap('n', 'Q', '<nop>')
 
 -- vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
