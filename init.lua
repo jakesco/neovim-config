42ad967ff07376dceaf9c33165c5ac32527ead46
Jake Scott <jscott@roguecu.org>
2023-08-01 08:44:49 -0700

    cleaned up telescope config

--- init.lua
+++ init.lua
@@ -155,10 +155,7 @@ require("lazy").setup({
       vim.cmd.colorscheme('catppuccin-macchiato')
     end,
   },
-  {
-    'folke/which-key.nvim',
-    opts = {}
-  },
+  { 'folke/which-key.nvim', opts = {} },
   {
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -191,19 +188,47 @@ require("lazy").setup({
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      defaults = {
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-u>'] = false,
+              ['<C-d>'] = false,
+            },
+          },
+          file_ignore_patterns = {
+            ".git",
           },
         },
-      },
-    },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      })
+      local builtin = require('telescope.builtin')
+      -- See `:help telescope.builtin`
+      mapkey('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+      mapkey('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      mapkey('n', '<leader>/', function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
+      mapkey('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+      mapkey('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      mapkey('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      mapkey('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      mapkey('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      mapkey('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    end
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
     build = 'make',
     cond = function()
       return vim.fn.executable 'make' == 1
@@ -215,6 +240,9 @@ require("lazy").setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
+    config = function()
+      pcall(require('telescope').load_extension, 'fzf')
+    end,
   },
   {
     'ThePrimeagen/harpoon',
@@ -259,45 +287,12 @@ require("lazy").setup({
 
 -- Stuff copied from kickstart
 -- TODO: clean this up
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp','lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
