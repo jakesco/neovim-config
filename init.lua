ccf1924836b37198fa7156deb0ec1981d9696ebe
Jake Scott <jscott@roguecu.org>
2023-08-21 10:38:11 -0700

    github theme

--- init.lua
+++ init.lua
@@ -133,11 +133,11 @@ require("lazy").setup({
   'tpope/vim-sleuth',
   'github/copilot.vim',
   {
-    'Mofiqul/adwaita.nvim',
+    'projekt0n/github-nvim-theme',
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('adwaita')
+      vim.cmd.colorscheme('github_dark')
     end,
   },
   {
