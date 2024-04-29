3aad6f3ced0ec4c578017544e6e0cd7b3a5df746
Jake Scott <jscott@roguecu.org>
2024-04-29 16:16:53 -0700

    formatting

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,9 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require('gruvbox').setup {
+    require('gruvbox').setup({
       contrast = 'soft',
-    }
-    vim.cmd.colorscheme 'gruvbox'
+    })
+    vim.cmd.colorscheme('gruvbox')
   end,
 }
