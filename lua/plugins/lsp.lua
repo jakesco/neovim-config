0bed195d085bbfaef811a8a8c8f36bdc04f71dc2
Jake Scott <jscott@roguecu.org>
2024-05-02 07:55:07 -0700

    enable pylsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,7 +99,7 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       gopls = {},
-      pyright = {},
+      pylsp = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes { ...},
